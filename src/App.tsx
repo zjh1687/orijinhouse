3d44f8f491e83c2d5faf24356dd183625310f849
seungcheol <zjh1687@naver.com>
2024-03-04 13:48:33 +0900

    메인 홈페이지 구성 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,12 @@
-import { FullPage, Slide } from 'react-full-page';
-import styled from 'styled-components';
-
-const Test = styled.video`
-  right: 0;
-  bottom: 0;
-  min-width: 100%;
-  min-height: 100%;
-  width: auto;
-  height: auto;
-  z-index: 100;
-  padding: 0;
-  background: url(polina.jpg) no-repeat;
-  background-size: cover;
-`;
-
 function App() {
   return (
-    <div className="text-[100px]">
-      <FullPage>
-        <Slide>
-          <Test autoPlay playsInline muted loop>
-            <source src="/1.mp4" type="video/mp4" />
-          </Test>
-        </Slide>
-        <Slide>
-          <Test autoPlay playsInline muted loop>
-            <source src="/2.mp4" type="video/mp4" />
-          </Test>
-        </Slide>
-        <Slide>
-          <Test autoPlay playsInline muted loop>
-            <source src="/3.mp4" type="video/mp4" />
-          </Test>
-        </Slide>
-        <Slide>
-          <Test autoPlay playsInline muted loop>
-            <source src="/4.mp4" type="video/mp4" />
-          </Test>
-        </Slide>
-        <Slide>
-          <iframe
-            src="https://my.spline.design/elegantbeautyofdarkaesthetics-0652af07349e1533a815ee2c4db6055f/"
-            width="100%"
-            height="100%"
-          ></iframe>
-        </Slide>
-      </FullPage>
+    <div className="text-[100px] w-full h-screen overflow-hidden">
+      <iframe
+        className="aspect-video"
+        src="https://my.spline.design/elegantbeautyofdarkaesthetics-0652af07349e1533a815ee2c4db6055f/"
+        width="100%"
+        height="100%"
+      ></iframe>
     </div>
   );
 }
