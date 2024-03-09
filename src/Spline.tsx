8f215624d9f3368b85e4fdce285569a3306edd6b
seungcheol <zjh1687@naver.com>
2024-03-09 16:51:01 +0900

    아이프레임 반응형 적용 및 스크롤 css추가

--- src/Spline.tsx
+++ src/Spline.tsx
@@ -1,13 +1,30 @@
+import { useEffect } from 'react';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+const IframeWrapper = styled.div`
+  ${tw`overflow-hidden relative `}
+`;
+
 function Spline() {
+  useEffect(() => {
+    document.body.classList.add('no-scroll');
+
+    return () => {
+      document.body.classList.remove('no-scroll');
+    };
+  }, []);
   return (
-    <div className="text-[100px] w-screen h-screen overflow-hidden">
-      <iframe
-        src="https://my.spline.design/w4mA0iriJQbYVzqSq27by8zK/"
-        width="100%"
-        height="100%"
-        className="border-none"
-      ></iframe>
-    </div>
+    <>
+      <IframeWrapper>
+        <div className="relative w-full h-0 pt-[56.25%]">
+          <iframe
+            src="https://my.spline.design/w4mA0iriJQbYVzqSq27by8zK/"
+            className="border-none w-full h-full absolute top-0 left-0"
+          ></iframe>
+        </div>
+      </IframeWrapper>
+    </>
   );
 }
 
